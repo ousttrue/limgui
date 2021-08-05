7328a7a3633cb2e7754faa450e7f850d19f03d3a
ousttrue <ousttrue@gmail.com>
2021-08-05 17:55:08 +0900

    Shader, VBO, Drawable

--- lua/limgui/renderer.lua
+++ lua/limgui/renderer.lua
@@ -4,8 +4,117 @@ local glfw = require("libs.gl_ffi.glfw")
 local M = {}
 local gl = require("libs.gl_ffi.mod")
 
+local vertex_shader_text = [[#version 110
+attribute vec2 vPos;
+void main()
+{
+    gl_Position = vec4(vPos, 0.0, 1.0);
+};
+]]
+
+local fragment_shader_text = [[#version 110
+void main()
+{
+    gl_FragColor = vec4(1, 1, 1, 1);
+};
+]]
+
+---@class VertexBuffer
+---@field vbo any
+---@field vertex_count number
+M.VBO = {
+    ---@param self VertexBuffer
+    render = function(self)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo[0])
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, self.vertex_count)
+    end,
+}
+
+M.VBO.create = function(vertices, vertex_count)
+    local vbo = ffi.new("GLuint[1]")
+    gl.glGenBuffers(1, vbo)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo[0])
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, ffi.sizeof(vertices), vertices, gl.GL_STATIC_DRAW)
+    return utils.new(M.VBO, {
+        vbo = vbo,
+        vertex_count = vertex_count,
+    })
+end
+
+---@class Shader
+---@field program any
+---@field vpos any
+M.Shader = {
+    activate = function(self)
+        -- set shader
+        gl.glUseProgram(self.program)
+        -- activate vertex slot
+        gl.glEnableVertexAttribArray(self.vpos)
+        -- FBO layout
+        gl.glVertexAttribPointer(self.vpos, 2, gl.GL_FLOAT, gl.GL_FALSE, 8, nil)
+    end,
+}
+M.Shader.create = function(vs, fs)
+    local vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+    local pp = ffi.new("const char *[1]")
+    pp[0] = vs
+    gl.glShaderSource(vertex_shader, 1, pp, nil)
+    gl.glCompileShader(vertex_shader)
+
+    local fragment_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
+    pp[0] = fs
+    gl.glShaderSource(fragment_shader, 1, pp, nil)
+    gl.glCompileShader(fragment_shader)
+
+    local program = gl.glCreateProgram()
+    gl.glAttachShader(program, vertex_shader)
+    gl.glAttachShader(program, fragment_shader)
+    gl.glLinkProgram(program)
+
+    local vpos_location = gl.glGetAttribLocation(program, "vPos")
+
+    return utils.new(M.Shader, {
+        program = program,
+        vpos = vpos_location,
+    })
+end
+
+---@class Drawable
+---@field vbo VertexBuffer
+---@field shader Shader
+M.Drawable = {
+    ---@param self Drawable
+    render = function(self)
+        self.shader:activate()
+        self.vbo:render()
+    end,
+}
+---@param scene Scene
+---@return Drawable
+M.Drawable.create = function(scene)
+    local shader = M.Shader.create(vertex_shader_text, fragment_shader_text)
+    local vbo = M.VBO.create(scene.vertices, scene.vertex_count)
+    return utils.new(M.Drawable, {
+        vbo = vbo,
+        shader = shader,
+    })
+end
+
 ---@class Renderer
+---@field drawable_map Table<Scene, Drawable>
 M.Renderer = {
+    ---@param self Renderer
+    ---@param scene Scene
+    ---@return Drawable
+    get_or_create_drawable = function(self, scene)
+        local drawable = self.drawable_map[scene]
+        if not drawable then
+            drawable = M.Drawable.create(scene)
+            self.drawable_map[scene] = drawable
+        end
+        return drawable
+    end,
+
     --- clear active RenderBuffer
     ---@param self Renderer
     ---@param w any
@@ -27,11 +136,14 @@ M.Renderer = {
     ---@param self Renderer
     ---@param scene Scene
     render = function(self, scene)
-        scene:render()
+        local drawable = self:get_or_create_drawable(scene)
+        drawable:render()
     end,
 }
 M.Renderer.new = function()
-    return utils.new(M.Renderer, {})
+    return utils.new(M.Renderer, {
+        drawable_map = {},
+    })
 end
 
 return M
--- lua/limgui/scene.lua
+++ lua/limgui/scene.lua
@@ -1,39 +1,13 @@
 local utils = require("limgui.utils")
 local ffi = require("ffi")
-local gl = require("libs.gl_ffi.mod")
 
 local M = {}
 
-local vertex_shader_text = [[#version 110
-attribute vec2 vPos;
-void main()
-{
-    gl_Position = vec4(vPos, 0.0, 1.0);
-};
-]]
-
-local fragment_shader_text = [[#version 110
-void main()
-{
-    gl_FragColor = vec4(1, 1, 1, 1);
-};
-]]
-
 ---@class Scene
----@field program any
+---@field vertices any
 ---@field vertex_count number
-M.Scene = {
-    render = function(self)
-        gl.glUseProgram(self.program)
-        --         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-        gl.glDrawArrays(gl.GL_TRIANGLES, 0, self.vertex_count)
-    end,
-}
+M.Scene = {}
 M.Scene.triangle = function()
-    local vertex_buffer = ffi.new("GLuint[1]")
-    gl.glGenBuffers(1, vertex_buffer)
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertex_buffer[0])
-
     -- static const struct
     -- {
     --     float x, y;
@@ -54,31 +28,9 @@ M.Scene.triangle = function()
     -- 2
     vertices[2][0] = 0.0
     vertices[2][1] = 0.6
-    gl.glBufferData(gl.GL_ARRAY_BUFFER, ffi.sizeof(vertices), vertices, gl.GL_STATIC_DRAW)
-
-    local vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
-    local pp = ffi.new("const char *[1]")
-    pp[0] = vertex_shader_text
-    gl.glShaderSource(vertex_shader, 1, pp, nil)
-    gl.glCompileShader(vertex_shader)
-
-    local fragment_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
-    pp[0] = fragment_shader_text
-    gl.glShaderSource(fragment_shader, 1, pp, nil)
-    gl.glCompileShader(fragment_shader)
-
-    local program = gl.glCreateProgram()
-    gl.glAttachShader(program, vertex_shader)
-    gl.glAttachShader(program, fragment_shader)
-    gl.glLinkProgram(program)
-
-    -- local mvp_location = glext.glGetUniformLocation(program, "MVP")
-    local vpos_location = gl.glGetAttribLocation(program, "vPos")
-    gl.glEnableVertexAttribArray(vpos_location)
-    gl.glVertexAttribPointer(vpos_location, 2, gl.GL_FLOAT, gl.GL_FALSE, 8, nil)
 
     return utils.new(M.Scene, {
-        program = program,
+        vertices = vertices,
         vertex_count = 3,
     })
 end
